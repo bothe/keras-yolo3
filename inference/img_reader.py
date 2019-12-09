b71cf40a35ffba201f92df2779836d2a2ea6677b
Chandrakant Bothe <bothe@foviatech.com>
2019-12-09 14:23:35 +0100

    inference module added: both yolo_v3 and its tiny

---
+++ inference/img_reader.py
@@ -0,0 +1,21 @@
+import numpy as np
+from PIL import Image
+
+from yolo3.utils import letterbox_image
+
+
+def read_img_infer(file, model_image_size):
+    org_image = Image.open(file)
+    if model_image_size != (None, None):
+        assert model_image_size[0] % 32 == 0, 'Multiples of 32 required'
+        assert model_image_size[1] % 32 == 0, 'Multiples of 32 required'
+        boxed_image = letterbox_image(org_image, tuple(reversed(model_image_size)))
+    else:
+        new_image_size = (org_image.width - (org_image.width % 32),
+                          org_image.height - (org_image.height % 32))
+        boxed_image = letterbox_image(org_image, new_image_size)
+    infer_image = np.array(boxed_image, dtype='float32')
+    infer_image /= 255.
+    infer_image = np.expand_dims(infer_image, 0)  # Add batch dimension.
+    print('Image shapes, org: {}, to infer: {}'.format(org_image.size, infer_image.shape))
+    return infer_image, org_image
--- inference_module.py
+++ inference_module.py
@@ -1,48 +1,37 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import numpy as np
 import tensorflow as tf
-from PIL import Image
 from tensorflow.python.platform import gfile
 
-from yolo3.utils import letterbox_image
+from inference.img_reader import read_img_infer
 
 file = 'test_data/frame66.jpg'
-
 model_image_size = (416, 416)
+infer_image, org_image = read_img_infer(file, model_image_size)
+tiny_yolo = False
 
-image = Image.open(file)
-img = tf.keras.preprocessing.image.load_img(file, target_size=[416, 416])
-
-if model_image_size != (None, None):
-    assert model_image_size[0] % 32 == 0, 'Multiples of 32 required'
-    assert model_image_size[1] % 32 == 0, 'Multiples of 32 required'
-    boxed_image = letterbox_image(image, tuple(reversed(model_image_size)))
+if tiny_yolo:
+    f = gfile.FastGFile("pb_models/yolov3-tiny.pb", 'rb')
 else:
-    new_image_size = (image.width - (image.width % 32),
-                      image.height - (image.height % 32))
-    boxed_image = letterbox_image(image, new_image_size)
-image_data = np.array(boxed_image, dtype='float32')
+    f = gfile.FastGFile("pb_models/yolo.pb", 'rb')
 
-print(image_data.shape)
-image_data /= 255.
-image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
-
-f = gfile.FastGFile("pb_models/yolov3-tiny.pb", 'rb')
 graph_def = tf.GraphDef()
 # Parses a serialized binary message into the current message.
 graph_def.ParseFromString(f.read())
 f.close()
 
 sess = tf.InteractiveSession()
-
 sess.graph.as_default()
-
-# Import a serialized TensorFlow `GraphDef` protocol buffer
-# and place into the current default `Graph`.
 tf.import_graph_def(graph_def)
 
-output_tensors = [sess.graph.get_tensor_by_name('import/conv2d_10/BiasAdd:0'),
-                  sess.graph.get_tensor_by_name('import/conv2d_13/BiasAdd:0')]
+if tiny_yolo:
+    output_tensors = [sess.graph.get_tensor_by_name('import/conv2d_10/BiasAdd:0'),
+                      sess.graph.get_tensor_by_name('import/conv2d_13/BiasAdd:0')]
+else:
+    output_tensors = [sess.graph.get_tensor_by_name('import/conv2d_59/BiasAdd:0'),
+                      sess.graph.get_tensor_by_name('import/conv2d_67/BiasAdd:0'),
+                      sess.graph.get_tensor_by_name('import/conv2d_75/BiasAdd:0')]
 
-predictions = sess.run(output_tensors, {'import/input_1:0': image_data})
+predictions = sess.run(output_tensors, {'import/input_1:0': infer_image})
+
+
