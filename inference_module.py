142c7a925b5b1f9ef44ddbafd4f8b1f38a88c868
Chandrakant Bothe <bothe@foviatech.com>
2019-12-11 14:58:18 +0100

    under-work

--- inference_module.py
+++ inference_module.py
@@ -2,10 +2,13 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import numpy as np
 import tensorflow as tf
+from PIL.Image import fromarray
 from matplotlib import pyplot as plt
 
 from inference.img_reader import read_img_infer
 from inference.utils import decode_netout, draw_boxes
+from utils.utils import postprocess_boxes, nms, draw_bbox
+from yolo3.model import yolo_boxes_and_scores
 from yolo_detection_cv_utils import get_anchors, load_classes
 
 # Load names of classes
@@ -16,6 +19,7 @@ tiny_yolo = False
 file = 'test_data/frame66.jpg'
 model_image_size = (416, 416)
 infer_image, org_image = read_img_infer(file, model_image_size)
+org_image_shape = (org_image.shape[0], org_image.shape[1])
 
 if tiny_yolo:
     f = tf.gfile.GFile("pb_models/yolov3-tiny.pb", 'rb')
@@ -41,8 +45,41 @@ else:
                       sess.graph.get_tensor_by_name('import/conv2d_75/BiasAdd:0')]
 
 predictions = sess.run(output_tensors, {'import/input_1:0': infer_image})
+net_out = predictions[0]
+n_shape = net_out.shape
+net_out_reshaped = np.reshape(net_out, (1, n_shape[1], n_shape[2], 3, 5 + nb_classes))
+features = tf.convert_to_tensor(net_out_reshaped)
+
 num_layers = len(predictions)
 anchor_mask = [[6, 7, 8], [3, 4, 5], [0, 1, 2]] if num_layers == 3 else [[3, 4, 5], [1, 2, 3]]  # default setting
+
+all_boxes = []
+for l in range(num_layers):
+    net_out = predictions[l]
+    n_shape = net_out.shape
+    net_out_reshaped = np.reshape(net_out, (1, n_shape[1], n_shape[2], 3, 5 + nb_classes))
+    features = tf.convert_to_tensor(net_out_reshaped)
+    x = tf.Session().run(yolo_boxes_and_scores(features, anchors[anchor_mask[0]], nb_classes,
+                                               model_image_size, org_image_shape))
+    boxes = np.concatenate([x[0], np.reshape(x[2][0], (n_shape[1]*n_shape[1]*3, 1)), x[1]], axis=1)
+    all_boxes.extend(boxes)
+
+bboxes = postprocess_boxes(all_boxes, org_image, model_image_size[0], 0.3)
+bboxes = nms(bboxes, 0.45, method='nms')
+image = draw_bbox(org_image, bboxes, classes)
+image = fromarray(image)
+image.show()
+
+#########################################################################################################
+pred_bbox = np.concatenate([np.reshape(predictions[0], (-1, 5 + nb_classes)),
+                            np.reshape(predictions[0], (-1, 5 + nb_classes)),
+                            np.reshape(predictions[0], (-1, 5 + nb_classes))], axis=0)
+
+bboxes = postprocess_boxes(pred_bbox, org_image, model_image_size[0], 0.3)
+bboxes = nms(bboxes, 0.45, method='nms')
+image = draw_bbox(org_image, bboxes, classes)
+image = fromarray(image)
+image.show()
 
 all_boxes = []
 for l in range(num_layers):
--- yolo3/model.py
+++ yolo3/model.py
@@ -154,7 +154,7 @@ def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
 
     if calc_loss:
         return grid, feats, box_xy, box_wh
-    return box_xy, box_wh, box_confidence, box_class_probs, feats
+    return box_xy, box_wh, box_confidence, box_class_probs
 
 
 def yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape):
@@ -185,13 +185,13 @@ def yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape):
 
 def yolo_boxes_and_scores(feats, anchors, num_classes, input_shape, image_shape):
     """Process Conv layer output"""
-    box_xy, box_wh, box_confidence, box_class_probs, features = yolo_head(feats,
+    box_xy, box_wh, box_confidence, box_class_probs = yolo_head(feats,
                                                                 anchors, num_classes, input_shape)
     boxes = yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape)
     boxes = K.reshape(boxes, [-1, 4])
     box_scores = box_confidence * box_class_probs
     box_scores = K.reshape(box_scores, [-1, num_classes])
-    return boxes, box_scores, features
+    return boxes, box_scores, box_confidence
 
 
 def yolo_eval(yolo_outputs,
@@ -208,8 +208,8 @@ def yolo_eval(yolo_outputs,
     boxes = []
     box_scores = []
     for l in range(num_layers):
-        _boxes, _box_scores, features = yolo_boxes_and_scores(yolo_outputs[l], anchors[anchor_mask[l]],
-                                                              num_classes, input_shape, image_shape)
+        _boxes, _box_scores, box_confidence = yolo_boxes_and_scores(yolo_outputs[l], anchors[anchor_mask[l]],
+                                                                    num_classes, input_shape, image_shape)
         boxes.append(_boxes)
         box_scores.append(_box_scores)
     boxes = K.concatenate(boxes, axis=0)
@@ -236,7 +236,7 @@ def yolo_eval(yolo_outputs,
     scores_ = K.concatenate(scores_, axis=0)
     classes_ = K.concatenate(classes_, axis=0)
 
-    return boxes_, scores_, classes_, yolo_outputs, features
+    return boxes_, scores_, classes_, yolo_outputs, box_confidence
 
 
 def preprocess_true_boxes(true_boxes, input_shape, anchors, num_classes):
